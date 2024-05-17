bf069b5a9ec3a9ba1952eb81eb8d87d1b7040208
MyLeatherDoesNotLeak <168596627+MyLeatherDoesNotLeak@users.noreply.github.com>
2024-05-17 14:28:13 +1200

    Add files via upload

---
+++ input output2.py
@@ -0,0 +1,14 @@
+username = input("What is your name?")
+fav_number = int(input(" What is your favourite number?"))
+
+double = fav_number * 2
+half = fav_number / 2
+square = fav_number * fav_number
+square_root = fav_number / fav_number
+
+print(f"Hi {username}, your favourit number is {fav_number}")
+
+print(f"Double of  {fav_number} is {double}")
+print(f"Half of  {fav_number} is {half}")
+print(f"Square of  {fav_number} is {square}")
+print(f"Square Root of  {fav_number} is {square_root}")
